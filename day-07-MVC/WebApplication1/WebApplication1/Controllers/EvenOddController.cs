1f6597d2a65cb90d010ac9b216dd048fb94ae501
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2021-12-31 15:40:53 +0530

    even odd check application using mvc empty

---
+++ day-07-MVC/WebApplication1/WebApplication1/Controllers/EvenOddController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EvenOddController : Controller
+    {
+        // GET: EvenOdd
+        public ActionResult Index()
+        {
+            var v = new EvenOdd();
+            v.Msg = "";
+            return View(v);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string a)
+        {
+            var v = new EvenOdd();
+            int num = int.Parse(a);
+            if ((num % 2) == 0)
+            {
+                v.Msg = "Number is Even";
+            }
+            else
+            {
+                v.Msg = "Number is Odd";
+            }
+         
+            return View(v);
+        }
+
+
+
+    }
+}

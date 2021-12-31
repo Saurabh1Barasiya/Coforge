afee408a4171ee7469811f20e44f6856a2a621fa
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2021-12-31 18:39:57 +0530

    MVC

---
+++ day-07-MVC/DisplayData/DisplayData/Controllers/ShowDataController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DisplayData.Models;
+
+namespace DisplayData.Controllers
+{
+    public class ShowDataController : Controller
+    {
+        // GET: ShowData
+        public ActionResult Index()
+        {
+            Student s = new Student()
+            {
+                Sid=1,Sname="Saurabh",Saddress="ward no 08",Scity="harrai"
+            };
+            ViewData["StudentData"] = s;
+            return View();
+        }
+    }
+}
---
+++ day-07-MVC/DisplayData/DisplayData/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DisplayData.Models
+{
+    public class Student
+    {
+        public int Sid { get; set; }
+        public string Sname { get; set; }
+        public string Saddress { get; set; }
+        public string Scity { get; set; }
+    }
+}

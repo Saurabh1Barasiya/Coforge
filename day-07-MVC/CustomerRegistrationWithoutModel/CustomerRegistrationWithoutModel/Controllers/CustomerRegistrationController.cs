4835b39129de89a1558b0e7faebbebbb41f8295e
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-01-02 21:03:12 +0530

    Data  saved on data based without Model Class

---
+++ day-07-MVC/CustomerRegistrationWithoutModel/CustomerRegistrationWithoutModel/Controllers/CustomerRegistrationController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CustomerRegistrationWithoutModel.Controllers
+{
+    public class CustomerRegistrationController : Controller
+    {
+        // GET: CustomerRegistration
+        public ActionResult SaveData()
+        {
+            ViewBag.result = "";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SaveData(string Cname,string Cemail,string Ccity)
+        {
+            string query = "insert into CustoerDetails(Cname,Cemail,Ccity) values('"+Cname+ "','" + Cemail + "','" + Ccity + "')";
+
+            string mycon = @"Data Source=DESKTOP-3BLA6L2\SQLEXPRESS; Initial Catalog=Registration; Integrated Security=true";
+
+            SqlConnection con = new SqlConnection(mycon);
+            con.Open();
+            SqlCommand cmd = new SqlCommand
+            {
+                CommandText = query,
+                Connection = con,
+            };
+
+
+            cmd.ExecuteNonQuery();
+            ViewBag.Result = "Data has been Saved";
+            return View();
+        }
+
+    }
+}
---
+++ day-07-MVC/CustomerRegistrationWithoutModel/CustomerRegistrationWithoutModel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CustomerRegistrationWithoutModel.MvcApplication" Language="C#" %>

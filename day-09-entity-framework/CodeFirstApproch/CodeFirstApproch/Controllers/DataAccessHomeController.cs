5488da2174f50310318ad18a1dc390cbfb4dc8a9
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2022-01-03 17:39:29 +0530

    Code first approch

---
+++ day-09-entity-framework/CodeFirstApproch/CodeFirstApproch/Controllers/DataAccessHomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeFirstApproch.Controllers
+{
+    public class DataAccessHomeController : Controller
+    {
+        // GET: DataAccessHome
+        public ActionResult Index()
+        {
+            RegistrationEntities entity = new RegistrationEntities();
+
+            return View(from CustoerDetail in entity.CustoerDetails select CustoerDetail);
+        }
+    }
+}
